f075ccd9e11af7383c940a08a44cd35808459a0e
DannyMyles <dannymyles1999@gmail.com>
2023-09-19 22:12:33 +0300

    Adds login aoi

--- ChisfisNextjs/src/app/auth/login/page.tsx
+++ ChisfisNextjs/src/app/auth/login/page.tsx
@@ -1,187 +1,5 @@
-// "use client";
-// import React, { FC, useState } from "react";
-// import facebookSvg from "@/images/Facebook.svg";
-// import twitterSvg from "@/images/Twitter.svg";
-// import googleSvg from "@/images/Google.svg";
-// import Input from "@/shared/Input";
-// import ButtonPrimary from "@/shared/ButtonPrimary";
-// import Image from "next/image";
-// import Link from "next/link";
-// import { useGoogleLogin } from "@/hooks/providers/useGoogleLogin";
-// import { useAppleLogin } from "@/hooks/providers/useAppleLogin";
-// import { useFacebookLogin } from "@/hooks/providers/useFacebookLogin";
-// import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-// import { app } from "config/firebase";
-// import axios from "axios";
-
-// export interface PageLoginProps {}
-
-
-// const PageLogin: FC<PageLoginProps> = ({}) => {
-//   const { googleLogin, isLoading: googleLoggining } = useGoogleLogin();
-//   const { appleLogin, isLoading: appleLoading } = useAppleLogin();
-//   const { facebookLogin, isLoading: facebookLoading } = useFacebookLogin();
-//   const [userDetails, setUserDetails] = useState({ email: "", password: "" });
-
-//   const loginUser = async (e: React.FormEvent<HTMLFormElement>) => {
-//     const auth = getAuth(app);
-//     e.preventDefault();
-//     try {
-//       const userCredential = await signInWithEmailAndPassword(
-//         auth,
-//         userDetails.email,
-//         userDetails.password
-//       );
-//       const user = userCredential.user;
-//       const token = await user?.getIdToken();
-
-//       const response = await axios.post(
-//         `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`,
-//         {
-//           token,
-//           password: userDetails.password,
-//         }
-//       );
-//       console.log(response);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-
-//   return (
-//     <div className={`nc-PageLogin`}>
-  
-//       <div className="container mb-24 lg:mb-32">
-//         <h2 className="my-20 flex items-center text-3xl leading-[115%] md:text-5xl md:leading-[115%] font-semibold text-neutral-900 dark:text-neutral-100 justify-center">
-//           Login
-//         </h2>
-//         <div className="max-w-md mx-auto space-y-6">
-//           <div className="grid gap-3">
-//             <button
-//               className="nc-will-change-transform flex w-full rounded-lg bg-primary-50 dark:bg-neutral-800 px-4 py-3 transform transition-transform sm:px-6 hover:translate-y-[-2px]"
-//               onClick={() => googleLogin()}
-//               disabled={googleLoggining}
-//             >
-//               <Image
-//                 className="flex-shrink-0"
-//                 src={googleSvg}
-//                 alt="Continue with Google"
-//               />
-//               <h3 className="flex-grow text-center text-sm font-medium text-neutral-700 dark:text-neutral-300 sm:text-sm">
-//                 {googleLoggining ? (
-//                   <div className="flex items-center justify-center">
-//                     <div className="w-4 h-4 border-t-2 border-r-2 border-gray-900 rounded-full animate-spin"></div>
-//                   </div>
-//                 ) : (
-//                   "Continue with Google"
-//                 )}
-//               </h3>
-//             </button>
-//             <button
-//               className="nc-will-change-transform flex w-full rounded-lg bg-primary-50 dark:bg-neutral-800 px-4 py-3 transform transition-transform sm:px-6 hover:translate-y-[-2px]"
-//               onClick={() => facebookLogin()}
-//               disabled={facebookLoading}
-//             >
-//               <Image
-//                 className="flex-shrink-0"
-//                 src={facebookSvg}
-//                 alt="Continue with Facebook"
-//               />
-//               <h3 className="flex-grow text-center text-sm font-medium text-neutral-700 dark:text-neutral-300 sm:text-sm">
-//                 {facebookLoading ? (
-//                   <div className="flex items-center justify-center">
-//                     <div className="w-4 h-4 border-t-2 border-r-2 border-gray-900 rounded-full animate-spin"></div>
-//                   </div>
-//                 ) : (
-//                   "Continue with Facebook"
-//                 )}
-//               </h3>
-//             </button>
-//             <button
-//               className="nc-will-change-transform flex w-full rounded-lg bg-primary-50 dark:bg-neutral-800 px-4 py-3 transform transition-transform sm:px-6 hover:translate-y-[-2px]"
-//               onClick={() => appleLogin()}
-//               disabled={appleLoading}
-//             >
-//               <Image
-//                 className="flex-shrink-0"
-//                 src={twitterSvg}
-//                 alt="Continue with Apple"
-//               />
-//               <h3 className="flex-grow text-center text-sm font-medium text-neutral-700 dark:text-neutral-300 sm:text-sm">
-//                 {appleLoading ? (
-//                   <div className="flex items-center justify-center">
-//                     <div className="w-4 h-4 border-t-2 border-r-2 border-gray-900 rounded-full animate-spin"></div>
-//                   </div>
-//                 ) : (
-//                   "Continue with Apple"
-//                 )}
-//               </h3>
-//             </button>
-//           </div>
-//           {/* OR */}
-//           <div className="relative text-center">
-//             <span className="relative z-10 inline-block px-4 font-medium text-sm bg-white dark:text-neutral-400 dark:bg-neutral-900">
-//               OR
-//             </span>
-//             <div className="absolute left-0 w-full top-1/2 transform -translate-y-1/2 border border-neutral-100 dark:border-neutral-800"></div>
-//           </div>
-//           {/* FORM */}
-//           <form
-//             className="grid grid-cols-1 gap-6"
-//             onSubmit={loginUser}
-//             method="post"
-//           >
-//             <label className="block">
-//               <span className="text-neutral-800 dark:text-neutral-200">
-//                 Email address
-//               </span>
-//               <Input
-//                 type="email"
-//                 onChange={(e) =>
-//                   setUserDetails({ ...userDetails, email: e.target.value })
-//                 }
-//                 value={userDetails.email}
-//                 placeholder="example@example.com"
-//                 className="mt-1"
-//               />
-//             </label>
-//             <label className="block">
-//               <span className="flex justify-between items-center text-neutral-800 dark:text-neutral-200">
-//                 Password
-//                 <Link
-//                   href="/auth/login"
-//                   className="text-sm underline font-medium"
-//                 >
-//                   Forgot password?
-//                 </Link>
-//               </span>
-//               <Input
-//                 type="password"
-//                 className="mt-1"
-//                 onChange={(e) =>
-//                   setUserDetails({ ...userDetails, password: e.target.value })
-//                 }
-//                 value={userDetails.password}
-//               />
-//             </label>
-//             <ButtonPrimary type="submit">Continue</ButtonPrimary>
-//           </form>
-
-//           {/* ==== */}
-//           <span className="block text-center text-neutral-700 dark:text-neutral-300">
-//             New user? {` `}
-//             <Link href="/auth/signup" className="font-semibold underline">
-//               Create an account
-//             </Link>
-//           </span>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PageLogin;
 "use client"
+
 import Head from "next/head";
 import Link from "next/link";
 import { useState } from "react";
@@ -192,7 +10,7 @@ import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
 
 import Input from "@/UI/Input/Input";
-// import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { AiFillApple } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 import { useMutation } from "@tanstack/react-query";
@@ -212,7 +30,7 @@ type LoginUser = {
   rememberMe: boolean;
 };
 
-export default function Login() {
+const Login = () => {
   const [authMethod, setAuthMethod] = useState("custom_phone");
   const { toast, sendSuccessToast, sendErrorToast } = useToast();
   const {
@@ -228,7 +46,7 @@ export default function Login() {
 
   const { appleLogin, appleToast, isLoading: appleLoading } = useAppleLogin();
 
-  // const router = useRouter();
+  const router = useRouter();
 
   const [loginUser, setLoginUser] = useState<LoginUser>({
     mobile: "",
@@ -252,7 +70,7 @@ export default function Login() {
         localStorage.setItem("user_id", data.data.id);
         localStorage.setItem("user_info", JSON.stringify(data.data));
         setTimeout(() => {
-          // router.push("/");
+          router.push("/");
         }, 1000);
       }
     },
@@ -260,7 +78,6 @@ export default function Login() {
       sendErrorToast("Error", error.response.data.data.error);
     },
   });
-
   const checkDisabled = (user: LoginUser) => {
     if (isLoading) return true;
     if (authMethod === "custom_phone") {
@@ -407,7 +224,7 @@ export default function Login() {
                   <div>
                     <button
                       type="submit"
-                      className="auth-button bg-purple disabled:bg-gray-50/30"
+                      className="auth-button bg-gray-300 p-4  w-[100%] rounded-full "
                       disabled={checkDisabled(loginUser)}
                     >
                       {isLoading ? (
@@ -427,7 +244,7 @@ export default function Login() {
 
                     {authMethod === "custom_phone" ? (
                       <h5
-                        className="text-center text-purple mt-3 text-[0.9rem] cursor-pointer"
+                        className="text-center  text-blue-800  mt-3 text-[0.9rem] cursor-pointer"
                         onClick={() => setAuthMethod("custom_email")}
                       >
                         Login with Email
@@ -447,7 +264,7 @@ export default function Login() {
                         type="button"
                         disabled={googleLoggining}
                         onClick={() => googleLogin()}
-                        className="auth-button text-black bg-white"
+                        className="auth-button text-black w-[100%] p-4  bg-red-300 rounded-full"
                       >
                         <div className={styles.apple}>
                           <FcGoogle size={25} />
@@ -462,7 +279,7 @@ export default function Login() {
                         type="button"
                         disabled={facebookLoading}
                         onClick={() => facebookLogin()}
-                        className="auth-button bg-[#0C86EF]"
+                        className="auth-button bg-[#0C86EF] p-4  w-[100%] rounded-full "
                       >
                         <div className={styles.apple}>
                           <FaFacebook size={25} />
@@ -478,7 +295,7 @@ export default function Login() {
                         type="button"
                         disabled={appleLoading}
                         onClick={() => appleLogin()}
-                        className="auth-button bg-black"
+                        className="auth-button bg-black text-white p-4  w-[100%] rounded-full "
                       >
                         <div className={styles.apple}>
                           <AiFillApple size={25} />
@@ -504,3 +321,5 @@ export default function Login() {
     </div>
   );
 }
+
+export default Login
